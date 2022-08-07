c27ef31c0e077291c717a8d21fc924fdb726bd28
shelestdan <daniil.shelesteev@yandex.ru>
2022-08-08 01:31:42 +0300

    +testMove

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,35 +5,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speedPlayer;
-    private Rigidbody2D _playerRb;
-    private Vector2 _playerMove;
-    private float jumpForce = 5000f;
+    private Rigidbody2D _pRb;
+    private float jumpForce = 10f;
     public bool onGround = true;
     public Transform GroundCheck;
     public float checkRadius = 0.5f;
     public LayerMask Ground;
+    private float moveInput;
 
 
     private void Awake()
     {
-        speedPlayer *= Time.deltaTime;
-        _playerRb = GetComponent<Rigidbody2D>();
-        _playerRb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        _playerRb.isKinematic = false;
+        _pRb = GetComponent<Rigidbody2D>();
+        _pRb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        _pRb.isKinematic = false;
     }
 
     private void FixedUpdate()
     {
-        _playerRb.velocity = _playerMove * speedPlayer;
+        moveInput = Input.GetAxis("Horizontal");
+        _pRb.velocity = new Vector2(moveInput * speedPlayer, _pRb.velocity.y);
     }
 
     private void Update()
     {
-        _playerMove.x = Input.GetAxis("Horizontal");
         if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
-            _playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+            _pRb.velocity = Vector2.up * jumpForce;
         }
+        
         CheckingGround();
     }
 
